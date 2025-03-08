d32cda78f4027f7058118ef696b21e2d45a43ad4
Pyunyu <valeryquijada@gmail.com>
2025-03-08 10:09:27 -0400

    actualizacion

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 # This file contains the SQLAlchemy models for the Flask API.
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import declarative_base, relationship
+import datetime
 
 db = declarative_base()
 
@@ -32,3 +33,84 @@ class Address(db):
 
     def __repr__(self):
         return f"<Address(email_address={self.email_address})>"
+
+
+class Product(db):
+     """
+     Represents the products in the database.
+     """
+     __tablename__ = "product"
+     id = Column(Integer, primary_key=True)
+     product = Column(String)
+     price = Column(String)
+     description = Column(String)
+     stock = Column(Integer)
+     straykidsmember = Column(Integer)
+     color = Column(String)
+ 
+     def __repr__(self):
+         return f"<User(product={self.product},price={self.price},description={self.description},stock={self.stock},straykidsmember={self.straykidsmember},color={self.color} ,)>"
+     
+class Cart(db):
+     
+     """
+     Represents the cart of the buyer in the database.
+     """
+     __tablename__ = "cart"
+     id = Column(Integer, primary_key=True)
+     creation_date = Column(Date, default=datetime.datetime.utcnow)  # Automatically set the creation date
+ 
+     def __repr__(self):
+         return f"<Cart(creation_date={self.creation_date}, relation_cart_item={self.relation_cart_item})>"
+     
+class CartItem(db):
+     """
+     Represents the CartItems of the buyer in the database.
+     """
+     __tablename__ = "cart_items"
+     id = Column(Integer, primary_key=True)
+     quantity = Column(Integer)
+ 
+     cart_id = Column(Integer, ForeignKey("cart.id"))
+     cart = relationship(Cart, backref="cart_items")
+ 
+     products_id = Column(Integer, ForeignKey("product.id"))
+     products = relationship(Product)
+ 
+     def __repr__(self):
+         return f"<CartItem(relation_cart={self.relation_cart}, relation_product={self.relation_product}, quantity={self.quantity})>"    
+     
+class Order(db):
+     """
+     Represents the Order of the buyer in the database.
+     """
+     __tablename__ = "order"
+     id = Column(Integer, primary_key=True)
+     creation_date = Column(Date, default=datetime.datetime.utcnow)
+     total_ammount = Column(Integer)
+     status = Column(String)
+     client_info = Column(String)
+     
+     def __repr__(self):
+         return f"<Order(creation_date={self.creation_date},total_ammount={self.total_ammount},status={self.status},client_info={self.client_info},relation_info{self.relation_info})>"
+ 
+class OrderItem(db):
+     """
+     Represents the Order Items of the buyer in the database.
+     """
+     __tablename__ = "order_items"
+     id = Column(Integer, primary_key=True)
+     quantity = Column(Integer)
+     price = Column(Integer)
+ 
+     order_id = Column(Integer, ForeignKey("order.id"))
+     order = relationship(Order, backref="order_items")
+ 
+     products_id = Column(Integer, ForeignKey("product.id"))
+     products = relationship(Product)
+ 
+def __repr__(self):
+         return f"<Order(relation_order={self.relation_order},relation_product={self.relation_product},quantity={self.quantity},price={self.price})>"
+
+if __name__ == "__main__":
+     tamagotchi = Product(product="wolfchan", price="35", description="Skzoo x Tamagotchi Wolfchan", stock="30", straykidsmember="Bangchan", color="gray")
